6288ce84d125ada6bc563202e4fef2b0a0a1867e
Yuriy Tataryntsev <yuriy_Tataryntsev@epam.com>
2018-02-27 16:18:01 +0200

    typo fix

--- src/features/ProductList/Product/Product.js
+++ src/features/ProductList/Product/Product.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Button from '../../../Components/Button/Button';
+import Button from '../../../components/Button/Button';
 import styles from './product.module.scss';
 
 const Product = (props) => {
